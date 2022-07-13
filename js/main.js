99cd80b11bde7a4cecbcf9f20dfdaee9474149dd
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-07-13 14:44:11 -0500

    Add comments to javscript. Add CSS spacing on main button and main image

--- js/main.js
+++ js/main.js
@@ -70,50 +70,6 @@ async function getBreweries(pageNo = 1, state){
   }
 
 
-
-// function getState(){
-//   console.log('change event fired!!');
-//   const stateChoice = document.querySelector('#state').value;
-//   const pageNum = 1;
-//   const select = document.querySelector('#city');
-
-//   const urlState = `https://api.openbrewerydb.org/breweries?per_page=50&page=${pageNum}&by_state=${stateChoice}`;
- 
-
-//   fetch(urlState)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data)
-
-//         //extract all the unique cities for the given state input
-//         const breweryCities = data.map(
-//           (obj) => {
-//             return obj.city;
-//           }
-//         ).filter(
-//           (item, index, arr) => {
-//             return arr.indexOf(item) == index
-//           }
-//         );
-
-
-//         // Put the cities for a given state into a list of options the user can select
-//         for(let i=0; i < breweryCities.length; i++) {
-//           const option = document.createElement('option');
-//           option.value = breweryCities[i];
-//           option.innerText = breweryCities[i];
-//           select.appendChild(option); 
-//         }
-
-//         document.querySelector('#city-starter-option').innerText = 'Select a City';
-//         console.log(breweryCities);
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
-
-
 function getCity(){
   const stateChoice = document.querySelector('#state').value;
   const cityChoice = document.querySelector('#city').value;
@@ -169,15 +125,19 @@ class BreweryList {
 
       //create a new row with 4 cells in our table for name, type, street, and website
       let newRow = tableRef.insertRow(-1);
-      let newNCell = newRow.insertCell(0);
-      let newTCell = newRow.insertCell(1);
-      let newSCell = newRow.insertCell(2);
-      let newWCell = newRow.insertCell(3);
+      let newNCell = newRow.insertCell(0);    //insert new Name Cell into the table
+      let newTCell = newRow.insertCell(1);    //insert new Brewery Type Cell into the table
+      let newSCell = newRow.insertCell(2);    //insert new Street Cell into the table
+      let newSTCell = newRow.insertCell(3);   //insert new State Cell into the table     
+      let newCCell = newRow.insertCell(4);    //insert new City Cell into the table
+      let newWCell = newRow.insertCell(5);    //insert new Website Cell into the table
 
       //create the text node that will go into our new cells
       let newNText = document.createTextNode(this.breweryList[key].name);
       let newTText = document.createTextNode(this.breweryList[key].brewery_type);
       let newSText = document.createTextNode(this.breweryList[key].street);
+      let newSTText = document.createTextNode(this.breweryList[key].state);
+      let newCText = document.createTextNode(this.breweryList[key].city);
 
       //Create a website link for each brewery in our brewery list
       let newLink = document.createElement('a');
@@ -189,6 +149,8 @@ class BreweryList {
       newNCell.appendChild(newNText);
       newTCell.appendChild(newTText);
       newSCell.appendChild(newSText);
+      newSTCell.appendChild(newSTText);
+      newCCell.appendChild(newCText);
       newWCell.appendChild(newLink);
  
       console.log(this.breweryList[key].name);
@@ -196,7 +158,7 @@ class BreweryList {
   }
 }
 
-
+//Definition for the Breweryinfo class which holds the data we wish to display to users of the application
 class Breweryinfo {
   constructor(breweryData) {
     this.name = breweryData.name,
